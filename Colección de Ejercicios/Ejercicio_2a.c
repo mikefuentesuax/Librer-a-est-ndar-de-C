8db37b93780bd02f4066812a8f8bd57351a45525
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2023-12-19 00:35:23 +0000

    parte 2 b

--- Colección de Ejercicios/Ejercicio_2a.c
+++ Colección de Ejercicios/Ejercicio_2a.c
@@ -3,8 +3,27 @@
 
 int main(){
     char cadena1[] = "Hola";
-    char cadena2[] = "Mundo";
+    char cadena2[] = "Hola";
 
     int resultado = strcmp(cadena1, cadena2);
     
+    if (resultado == 0){
+        printf("Las cadenas son iguales\n");
+    } else {
+        printf("Las cadenas son diferentes\n");
     }
+
+    char cadena3[] = "Mundo";
+    char cadena4[] = "Hola";
+
+    int resultado2 = strcmp(cadena3, cadena4);
+
+    if (resultado2 == 0){
+        printf("Las cadenas son iguales\n");
+    } else {
+        printf("Las cadenas son diferentes\n");
+    }
+
+
+    return 0;
+}
