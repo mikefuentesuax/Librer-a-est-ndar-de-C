9b203a2038ab90143dc51747f337597edb91a53d
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2023-12-19 00:40:18 +0000

    Condiciones para que no se abra

--- Colección de Ejercicios/Ejercicio_3a.c
+++ Colección de Ejercicios/Ejercicio_3a.c
@@ -5,5 +5,16 @@ int main() {
 
     FILE *archivo = fopen(nombre_archivo, "r");
 
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo\n");
+        fclose(archivo);
+    } else {
+        printf("Se pudo abrir el archivo\n");
+        fclose(archivo);
+    }
+
+    return 0;
+
+
 
 }
