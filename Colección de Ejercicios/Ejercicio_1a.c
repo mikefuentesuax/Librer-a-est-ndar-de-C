fe8d8b84e3e6839e5fa414ca49e8509ba8f2f255
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2023-12-19 00:26:12 +0000

    Numeros positivos

--- Colección de Ejercicios/Ejercicio_1a.c
+++ Colección de Ejercicios/Ejercicio_1a.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 
 int main() {
-    // Declarar un número negativo
+    // Declarar un número negativo y positivo
     int num = -10;
+    int num_positivo = 8;
 
     // Obtener el valor absoluto utilizando abs()
     int abs_num = abs(num);
+    int abs_num_positivo = abs(num_positivo);
 
     printf("El valor absoluto de %d es %d\n", num, abs_num);
+    printf("El valor absoluto de %d es %d\n", num_positivo, abs_num_positivo);
 
     return 0;
 }
